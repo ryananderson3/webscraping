e34bfd7c086179bbf2c10672db85bf50f66a46a7
Ryan <ryan_anderson3@baylor.edu>
2022-11-15 15:44:20 -0600

    changes

--- webscraping_project.py
+++ webscraping_project.py
@@ -29,15 +29,20 @@ for x in range(5):
         symbol = currency[len(currency)-3:]
     price = str(table_cell[index+3].text).split('$')
 
+    num = price[1].replace(',','')
+    #gets the dollar amount that the crypto has changed that day
+    percent = table_cell[index+4].text
+    pChange = round(float(num) * float(percent[1:len(percent)-1]),2)
+
     print(f'Name: {name}')
     print(f'Symbol: {symbol}')
     print(f'Price: ${price[1]}')
     print(f'Day Change: {table_cell[index+4].text}')
+    print(f'Price Based on % Change: ${pChange}')
     print()
     index+=9
 
     #if Bitcoin falls under $40,000 or Etherium under $3000, send text message to phone with twilio
-    num = price[1].replace(',','')
     num = int(num[:-3])
     if name == 'Bitcoin' and num < 40000:
         text = 'ALERT:\n\n' + name + ' has fallen below $40,000 to $' + price[1]
@@ -46,5 +51,5 @@ for x in range(5):
         text = '\nALERT:\n\n' + name + ' has fallen below $3,000 to $' + price[1]
         textmessage = client.messages.create(to=myCellPhone, from_=TwilioNumber,body=text)
     
-    #input()
+    input()
 
